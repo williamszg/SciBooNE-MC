57619997910a3dd8a9443736674451ea59599ebf
Zack Williams <zachary.williams2@mavs.uta.edu>
2017-10-03 12:26:56 -0500

    Current Progress With Proton Mis-ID

---
+++ ProtonMisID/NewNMRSProton.C
@@ -0,0 +1,103 @@
+#define NewNMRSProton_cxx
+#include "NewNMRSProton.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TRandom.h>
+#include <math.h>
+#include <stdio.h>
+#define PI 3.14159265
+
+//-----------------------------|
+//---       Histograms      ---|
+//-----------------------------|
+
+TH1D *h = new TH1D("h", "", 100, 0, 100);
+
+//-----------------------------|
+
+
+//-----------------------------|
+//---  Constant Parameters  ---|
+//-----------------------------|
+
+int protonpdg = 2212;
+
+//-----------------------------|
+
+
+//===========================================|
+//              Event Selection              |
+//===========================================|
+
+void NewNMRSProton::Loop()
+{
+
+   if (fChain == 0) return;
+   Long64_t nentries = fChain->GetEntriesFast();
+   Long64_t nbytes = 0, nb = 0;
+
+   //----------------------------------------|
+   //---     Event Counters/Register      ---|
+   //----------------------------------------|
+
+   int nEvents = 0;
+   int nEventsWithProton = 0;
+   int nProtons = 0;
+   int ProtonRegister = 99;
+
+   //----------------------------------------|
+
+   for (Long64_t jentry=0; jentry<nentries; jentry++)
+   {
+
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+      nEvents++;
+      if (nEvents%1000 == 0) {std::cout<<"Event = "<<nEvents<<std::endl;} //Event print out every 1000 events
+
+      TRandom3 *randX = new TRandom3();
+      TRandom3 *randY = new TRandom3();
+      TRandom3 *flat = new TRandom3();
+      randX->SetSeed(jentry/2);
+      randY->SetSeed(jentry*jentry);
+      flat->SetSeed(jentry*jentry*jentry);
+      double Xpos = randX->Gaus(1.5,1.3);
+      while (Xpos<0 || Xpos>3.0) { Xpos = randX->Gaus(1.5,1.3); }
+      double Ypos = randY->Gaus(1.5,1.05);
+      while (Ypos<0 || Ypos>3.0) { Ypos = randY->Gaus(1.5,1.05); }
+      double Zpos = flat->Uniform(0,1.7);
+
+      for (int npart=0; npart<StdHepN; npart++)
+      {
+
+         if (StdHepPdg[npart] == protonpdg && StdHepStatus[npart] == 1) 
+	 {
+
+            nProtons++;
+	    ProtonRegister = npart;
+
+	 } //<--- Close if statement
+
+
+      } //<--- Close loop over particles in event
+
+
+      if (nProtons > 0) 
+      { 
+
+         nEventsWithProton++; 
+	 nProtons = 0;
+
+      } //<--- Close amount of events with proton loop
+
+
+   } //<--- Close jentry for loop
+
+
+   std::cout<<"Number of Events with at least a single proton in final state = "<<nEventsWithProton<<std::endl;
+} //<--- Close void loop
+
+//===========================================|
---
+++ ProtonMisID/OldNMRSProton.h
@@ -0,0 +1,153 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Oct  3 11:24:31 2017 by ROOT version 6.10/02
+// from TTree nRooTracker/Pure NEUT RooTracker
+// found on file: SciBooNE_numu_coh_OLDNEUT_RooTrack.root
+//////////////////////////////////////////////////////////
+
+#ifndef OldNMRSProton_h
+#define OldNMRSProton_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "TObjString.h"
+#include "TObject.h"
+
+class OldNMRSProton {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+ //TObjString      *EvtCode;
+   UInt_t          fUniqueID;
+   UInt_t          fBits;
+   TString         fString;
+   Int_t           EvtNum;
+   Int_t           StdHepN;
+   Int_t           StdHepPdg[22];   //[StdHepN]
+   Int_t           StdHepStatus[22];   //[StdHepN]
+   Double_t        StdHepP4[100][4];
+
+   // List of branches
+   TBranch        *b_EvtCode_fUniqueID;   //!
+   TBranch        *b_EvtCode_fBits;   //!
+   TBranch        *b_EvtCode_fString;   //!
+   TBranch        *b_EvtNum;   //!
+   TBranch        *b_StdHepN;   //!
+   TBranch        *b_StdHepPdg;   //!
+   TBranch        *b_StdHepStatus;   //!
+   TBranch        *b_StdHepP4;   //!
+
+   OldNMRSProton(TTree *tree=0);
+   virtual ~OldNMRSProton();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef OldNMRSProton_cxx
+OldNMRSProton::OldNMRSProton(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("../SciBooNE_numu_coh_OLDNEUT_RooTrack.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("../SciBooNE_numu_coh_OLDNEUT_RooTrack.root");
+      }
+      f->GetObject("nRooTracker",tree);
+
+   }
+   Init(tree);
+}
+
+OldNMRSProton::~OldNMRSProton()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t OldNMRSProton::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t OldNMRSProton::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void OldNMRSProton::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("fUniqueID", &fUniqueID, &b_EvtCode_fUniqueID);
+   fChain->SetBranchAddress("fBits", &fBits, &b_EvtCode_fBits);
+   fChain->SetBranchAddress("fString", &fString, &b_EvtCode_fString);
+   fChain->SetBranchAddress("EvtNum", &EvtNum, &b_EvtNum);
+   fChain->SetBranchAddress("StdHepN", &StdHepN, &b_StdHepN);
+   fChain->SetBranchAddress("StdHepPdg", StdHepPdg, &b_StdHepPdg);
+   fChain->SetBranchAddress("StdHepStatus", StdHepStatus, &b_StdHepStatus);
+   fChain->SetBranchAddress("StdHepP4", StdHepP4, &b_StdHepP4);
+   Notify();
+}
+
+Bool_t OldNMRSProton::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void OldNMRSProton::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t OldNMRSProton::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef OldNMRSProton_cxx
